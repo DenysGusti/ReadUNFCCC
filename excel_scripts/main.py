1bcc5ba98925b0fb46cd66240b35ef5efbe7ea7a
mykola-gusti <gusti@iiasa.ac.at>
2025-02-28 16:53:56 +0100

    UnZipping is added
    Now the downloaded zip files can be unzipped with the script.

--- excel_scripts/main.py
+++ excel_scripts/main.py
@@ -3,10 +3,14 @@ from settings import *
 from data_extraction import TableCreation
 from time_series import TimeSeries
 from auxiliary_functions import calculateTime
+from zip_extract import unzip_files
 
 
 @calculateTime
 def main() -> None:
+    if UNZIP_DATA:
+        unzip_files(DATA_PATH)
+
     if EXTRACT_DATA:
         for destination, sources in COUNTRIES_DICT.items():
             TableCreation(country_destination=destination, country_sources=sources, sheets_list=SHEETS_LIST)
